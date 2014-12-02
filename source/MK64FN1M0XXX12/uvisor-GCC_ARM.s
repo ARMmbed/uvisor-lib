6ee6fb1543088793fb07d138e95076a7375203e8
Milosch Meriac <milosch.meriac@arm.com>
2014-12-02 18:57:59 +0000

    new uvisor firmware added

---
+++ source/MK64FN1M0XXX12/uvisor-GCC_ARM.s
@@ -0,0 +1,17 @@
+.globl uvisor_init
+.type uvisor_init, %function
+.section .text.uvisor, "x"
+ .align 5
+ .thumb
+ .thumb_func
+uvisor_init:
+ .incbin "uvisor-MK64FN1M0XXX12.box"
+ .long 0x2FE539A6
+ .long 0
+ .long __descr_tbl_start
+ .long __descr_tbl_end
+ .align 5
+.section .bss.uvisor,"awM",@nobits
+ .align 5
+ .space 0x1000
+ .align 5
