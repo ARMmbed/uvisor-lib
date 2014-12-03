b688f20fd8288d8f5f42ce11cffd069479db7caa
Milosch Meriac <milosch.meriac@arm.com>
2014-12-03 22:01:20 +0000

    new uvisor version with enable/disable function

--- source/MK64FN1M0XXX12/uvisor-GCC_ARM.s
+++ source/MK64FN1M0XXX12/uvisor-GCC_ARM.s
@@ -8,8 +8,9 @@ uvisor_init:
  .incbin "uvisor-MK64FN1M0XXX12.box"
  .long 0x2FE539A6
  .long 0
- .long __descr_tbl_start
- .long __descr_tbl_end
+ .long 1
+ .long __uvisor_cfgtbl_start
+ .long __uvisor_cfgtbl_end
  .align 5
 .section .bss.uvisor,"awM",@nobits
  .align 5
