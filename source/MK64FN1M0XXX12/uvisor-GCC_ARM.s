258fb81384841e2e95ed4e84dbb0720f9ab38daa
Milosch Meriac <milosch.meriac@arm.com>
2014-12-04 15:54:56 +0000

    added uvisor.mode as a weak symbol

--- source/MK64FN1M0XXX12/uvisor-GCC_ARM.s
+++ source/MK64FN1M0XXX12/uvisor-GCC_ARM.s
@@ -1,16 +1,21 @@
 .globl uvisor_init
+.globl uvisor_config
 .type uvisor_init, %function
+.weak __uvisor_mode
 .section .text.uvisor, "x"
  .align 5
  .thumb
  .thumb_func
 uvisor_init:
  .incbin "uvisor-MK64FN1M0XXX12.box"
+uvisor_config:
  .long 0x2FE539A6
  .long 0
- .long 0
+ .long __uvisor_mode
  .long __uvisor_cfgtbl_start
  .long __uvisor_cfgtbl_end
+__uvisor_mode:
+ .long 0
  .align 5
 .section .bss.uvisor,"awM",@nobits
  .align 5
