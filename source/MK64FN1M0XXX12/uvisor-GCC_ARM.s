d706b80644f6023e1d2e696f0f30970b56dc5939
Alessandro Angelino <alessandro.angelino@arm.com>
2015-04-20 11:28:25 +0100

    Minor typos and tab fixes

--- source/MK64FN1M0XXX12/uvisor-GCC_ARM.s
+++ source/MK64FN1M0XXX12/uvisor-GCC_ARM.s
@@ -23,30 +23,30 @@
 .type uvisor_init, %function
 .weak __uvisor_mode
 .section .uvisor.main, "x"
- .thumb
- .thumb_func
+    .thumb
+    .thumb_func
 uvisor_init:
- .align 5
- .incbin "uvisor-MK64FN1M0XXX12.box"
+    .align 5
+    .incbin "uvisor-MK64FN1M0XXX12.box"
 uvisor_config:
- .long 0x2FE539A6
- .long 0
- .long __uvisor_mode
- .long __uvisor_cfgtbl_start
- .long __uvisor_cfgtbl_end
- .long __uvisor_data_src
- .long __uvisor_data_start
- .long __uvisor_data_end
- .long __uvisor_bss_start
- .long __uvisor_bss_end
- .long __uvisor_secure_start
- .long __uvisor_secure_end
- .long __uvisor_reserved_start
- .long __uvisor_reserved_end
+    .long 0x2FE539A6
+    .long 0
+    .long __uvisor_mode
+    .long __uvisor_cfgtbl_start
+    .long __uvisor_cfgtbl_end
+    .long __uvisor_data_src
+    .long __uvisor_data_start
+    .long __uvisor_data_end
+    .long __uvisor_bss_start
+    .long __uvisor_bss_end
+    .long __uvisor_secure_start
+    .long __uvisor_secure_end
+    .long __uvisor_reserved_start
+    .long __uvisor_reserved_end
 __uvisor_mode:
- .long 0
- .align 5
+    .long 0
+    .align 5
 .section .uvisor.bss.main,"awM",@nobits
 __uvisor_reserved_start:
- .space 0x2000
+    .space 0x2000
 __uvisor_reserved_end:
