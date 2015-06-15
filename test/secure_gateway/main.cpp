f027f86021e478d2be4ee915a4682589a3363432
Alessandro Angelino <alessandro.angelino@arm.com>
2015-06-15 17:02:44 +0100

    Added tests for basic secure gateway function calls
    Only one box used for now, testing function call with variable number of
    arguments

---
+++ test/secure_gateway/main.cpp
@@ -0,0 +1,26 @@
+/***************************************************************
+ * This confidential and  proprietary  software may be used only
+ * as authorised  by  a licensing  agreement  from  ARM  Limited
+ *
+ *             (C) COPYRIGHT 2013-2015 ARM Limited
+ *                      ALL RIGHTS RESERVED
+ *
+ *  The entire notice above must be reproduced on all authorised
+ *  copies and copies  may only be made to the  extent permitted
+ *  by a licensing agreement from ARM Limited.
+ *
+ ***************************************************************/
+#include "mbed/mbed.h"
+#include "uvisor-lib/uvisor-lib.h"
+
+/* main box ACLs */
+static const UvisorBoxAclItem g_main_acl[] = {
+    {MCG,                  sizeof(*MCG),       UVISOR_TACLDEF_PERIPH},
+    {UART0,                sizeof(*UART0),     UVISOR_TACLDEF_PERIPH},
+    {PIT,                  sizeof(*PIT),       UVISOR_TACLDEF_PERIPH},
+    {SIM,                  sizeof(*SIM),       UVISOR_TACLDEF_PERIPH},
+    {PORTB,                sizeof(*PORTB),     UVISOR_TACLDEF_PERIPH},
+};
+
+/* enable uvisor */
+UVISOR_SET_MODE_ACL(2, g_main_acl);
