d440df73175948e59b4877fba6b9400a782a8943
Alessandro Angelino <alessandro.angelino@arm.com>
2015-06-15 17:02:44 +0100

    Added tests for basic secure gateway function calls
    Only one box used for now, testing function call with variable number of
    arguments

---
+++ test/secure_gateway/box1.h
@@ -0,0 +1,22 @@
+/***************************************************************
+ * This confidential and  proprietary  software may be used only
+ * as authorised  by  a licensing  agreement  from  ARM  Limited
+ *
+ *             (C) COPYRIGHT 2013-2015 ARM Limited
+ *                      ALL RIGHTS RESERVED
+ *
+ *  The entire notice above must be reproduced on all authorised
+ *  copies and copies  may only be made to the  extent permitted
+ *  by a licensing agreement from ARM Limited.
+ *
+ ***************************************************************/
+#ifndef __BOX1_H__
+#define __BOX1_H__
+
+uint32_t box1_test_function(void);
+uint32_t box1_test_function(uint32_t a1);
+uint32_t box1_test_function(uint32_t a1, uint32_t a2);
+uint32_t box1_test_function(uint32_t a1, uint32_t a2, uint32_t a3);
+uint32_t box1_test_function(uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4);
+
+#endif/*__BOX1_H__*/
