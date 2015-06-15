bd8521d5355f593fa3d4b5a2b4ffffcba2b8d168
Alessandro Angelino <alessandro.angelino@arm.com>
2015-06-15 17:01:00 +0100

    Minor changes to IRQ tests
    Main test files renamed to box0_* format, for consistency with future tests;
    some header files inclusion changed as well

--- test/irq/box0_basic.cpp
+++ test/irq/box0_basic.cpp
@@ -11,20 +11,13 @@
  *
  ***************************************************************/
 #include "mbed/mbed.h"
-#include "mbed/test_env.h"
 #include "mbed-cpputest/CppUTest/TestHarness.h"
 #include "uvisor-lib/uvisor-lib.h"
 #include "common.h"
 
-#define TEST_IRQn 42
-#define TEST_PRIO 2
-#define TEST_VAL  1
-
-#define DFLT_PRIO 1
-
 void test_handler(void)
 {
-    g_flag = TEST_VAL;
+    g_flag = TEST1_VAL;
 }
 
 TEST_GROUP(IRQTestBasic)
@@ -36,8 +29,8 @@ TEST(IRQTestBasic, box0_isr_set_get)
     uint32_t hdlr;
 
     /* set ISR and check if it has been correctly set */
-    uvisor_isr_set(TEST_IRQn, (uint32_t) &test_handler, 0);
-    hdlr = uvisor_isr_get(TEST_IRQn);
+    uvisor_isr_set(TEST1_IRQn, (uint32_t) &test_handler, 0);
+    hdlr = uvisor_isr_get(TEST1_IRQn);
     CHECK_EQUAL((uint32_t) &test_handler, hdlr);
 }
 
@@ -46,25 +39,25 @@ TEST(IRQTestBasic, box0_irq_pending_clr_set_get)
     uint32_t pending;
 
     /* disable IRQ to make sure it does not get active */
-    uvisor_irq_disable(TEST_IRQn);
+    uvisor_irq_disable(TEST1_IRQn);
 
     /* clear pending IRQn, if any, and check clearing */
-    uvisor_irq_pending_clr(TEST_IRQn);
-    pending = uvisor_irq_pending_get(TEST_IRQn);
+    uvisor_irq_pending_clr(TEST1_IRQn);
+    pending = uvisor_irq_pending_get(TEST1_IRQn);
     CHECK_EQUAL(0, pending);
 
     g_flag = 0;
 
     /* set pending IRQn and check that its ISR has not been executed */
-    uvisor_irq_pending_set(TEST_IRQn);
+    uvisor_irq_pending_set(TEST1_IRQn);
     __DSB();
     __ISB();
     CHECK_EQUAL(0, g_flag);
 
     /* check the pending status and clear it  */
-    pending = uvisor_irq_pending_get(TEST_IRQn);
+    pending = uvisor_irq_pending_get(TEST1_IRQn);
     CHECK_EQUAL(1, pending);
-    uvisor_irq_pending_clr(TEST_IRQn);
+    uvisor_irq_pending_clr(TEST1_IRQn);
 }
 
 TEST(IRQTestBasic, box0_irq_ena_dis)
@@ -72,25 +65,25 @@ TEST(IRQTestBasic, box0_irq_ena_dis)
     g_flag = 0;
 
     /* configure the ISR and clear IRQn */
-    uvisor_isr_set(TEST_IRQn, (uint32_t) &test_handler, 0);
-    uvisor_irq_pending_clr(TEST_IRQn);
+    uvisor_isr_set(TEST1_IRQn, (uint32_t) &test_handler, 0);
+    uvisor_irq_pending_clr(TEST1_IRQn);
 
     /* enable and trigger IRQn */
-    uvisor_irq_enable(TEST_IRQn);
-    uvisor_irq_pending_set(TEST_IRQn);
+    uvisor_irq_enable(TEST1_IRQn);
+    uvisor_irq_pending_set(TEST1_IRQn);
     __DSB();
     __ISB();
-    CHECK_EQUAL(TEST_VAL, g_flag);
+    CHECK_EQUAL(TEST1_VAL, g_flag);
 
     g_flag = 0;
 
     /* disable IRQn and check it does not get triggered again */
-    uvisor_irq_disable(TEST_IRQn);
-    uvisor_irq_pending_set(TEST_IRQn);
+    uvisor_irq_disable(TEST1_IRQn);
+    uvisor_irq_pending_set(TEST1_IRQn);
     __DSB();
     __ISB();
     CHECK_EQUAL(0, g_flag);
-    uvisor_irq_pending_clr(TEST_IRQn);
+    uvisor_irq_pending_clr(TEST1_IRQn);
 }
 
 TEST(IRQTestBasic, box0_irq_priority_set_get)
@@ -98,10 +91,10 @@ TEST(IRQTestBasic, box0_irq_priority_set_get)
     uint32_t priority;
 
     /* set priority and check it has been set correctly */
-    uvisor_irq_priority_set(TEST_IRQn, TEST_PRIO);
-    priority = uvisor_irq_priority_get(TEST_IRQn);
-    CHECK_EQUAL(TEST_PRIO, priority);
+    uvisor_irq_priority_set(TEST1_IRQn, TEST1_PRIO);
+    priority = uvisor_irq_priority_get(TEST1_IRQn);
+    CHECK_EQUAL(TEST1_PRIO, priority);
 
     /* restore default priority */
-    uvisor_irq_priority_set(TEST_IRQn, DFLT_PRIO);
+    uvisor_irq_priority_set(TEST1_IRQn, DFLT_PRIO);
 }
